b9879636cca9a773dce3dbb84ce98c62c6818149
bjorn.ekryd <bjorn.ekryd@70864216-0395-11df-9cd7-c9a19219af24>
2010-01-25 21:57:12 +0000

    Påbörjat konvertering av GUI. Kompilerar, men går inte att köra.

---
+++ src/main/scala/zipfinder/gui/RecentStringsTextField.scala
@@ -0,0 +1,35 @@
+package zipfinder.gui
+
+import javax.swing.JTextField
+import javax.swing.text.AttributeSet
+import javax.swing.text.BadLocationException
+import javax.swing.text.Document
+import javax.swing.text.PlainDocument
+import swing._
+
+class RecentStringsTextField(protected val recentStringsToFind:Array[String]) extends TextField {
+
+	override lazy val peer: JTextField = new JTextField("", 0) with SuperMixin {
+		override protected def createDefaultModel = new UpperCaseDocument
+ 	}
+
+	private def getCurrentText = super.text
+
+	class UpperCaseDocument extends PlainDocument {
+		override def replace(offset:Int, length:Int, text:String, attrs:AttributeSet) {
+			super.replace(offset, length, text, attrs)
+			if (text == null || text.length == 0) {
+				return
+			}
+			val currentText = getCurrentText
+			for (recentString <- recentStringsToFind) {
+				if (recentString.startsWith(currentText) && recentString.length > currentText.length) {
+					super.replace(0, currentText.length, recentString, attrs)
+					selectAll
+					peer.setSelectionStart(offset + 1)
+					return
+				}
+			}
+		}
+	}
+}
---
+++ src/test/scala/zipfinder/gui/RecentStringsTextFieldRunner.scala
@@ -0,0 +1,11 @@
+package zipfinder.gui
+
+import swing._
+
+object RecentStringsTextFieldRunner extends SimpleGUIApplication {
+	def top = new MainFrame {
+	  title = "LimitedFileChooser instance test"
+	  val instance = new RecentStringsTextField(Array( "aaaa", "bbbbb", "ccccc" ))
+      contents = instance
+	}
+}
